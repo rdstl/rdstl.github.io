8850747d2585d933063cb92508af8726c31a4b6d
Rolando Distel <rdstl7529@gmail.com>
2023-10-04 12:49:29 -0300

    se modificó el error de la card vacia en index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,17 @@
 function makeCards(idContainer)
 {
+   var container = document.getElementById("container");
+   
   for(let book of data.books)
-  {         
+  {
+           
       sendCard(book.image, book._id, book.name, book.description, book.date, book.price, idContainer);
+     
   }
 };
 
 makeCards('card-container');
 
+// la funcion makeCards genera una card y 
+// la envia a la funcion sendCard 
+// para que la envie al archivo index.html
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 
 function sendCard(cardImage, cardId, cardName, cardDescription, cardDate, cardPrice, cardIdContainer )
 {
-   let card = `<div class="cards">
-                 <a href="./details.html?id=${cardId}"><img class="img-card" src="${cardImage}" alt="book"></a>
-                
+   let card = `<div class="cards" id="card">
+                  <div class="cards">
+                  <a href="./details.html?id=${cardId}"><img class="img-card" src="${cardImage}" alt="book"></a>
+                  </div>
                </div>`;
 
    document.getElementById(cardIdContainer).innerHTML += card;
 }
+
+// la funcion sendCard envia la card al archivo index.html
