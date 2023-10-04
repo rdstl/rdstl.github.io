de65b5307b63a20c228fd908334af765eff56ead
Rolando Distel <rdstl7529@gmail.com>
2023-10-04 15:00:39 -0300

    se modificó el error del doble recuadro naranja de las cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,7 @@
 function sendCard(cardImage, cardId, cardName, cardDescription, cardDate, cardPrice, cardIdContainer )
 {
    let card = `<div class="cards" id="card">
-                  <div class="cards">
                   <a href="./details.html?id=${cardId}"><img class="img-card" src="${cardImage}" alt="book"></a>
-                  </div>
                </div>`;
 
    document.getElementById(cardIdContainer).innerHTML += card;
